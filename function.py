7e0436b8b18dc9614049ae25e89306fa6cf88401
Krishna Raut <krishnaraut4491@gmail.com>
2026-09-24 19:32:19 +0530

    modules added

---
+++ function.py
@@ -0,0 +1,24 @@
+#LAMDA 
+"""
+Syntax 
+lamda argument : expression
+"""
+# add = lambda a : a+1
+# addition = add (10)
+# print(addition)
+
+# add = lambda a , b: a+1+b
+# addition = add (10,14)
+# print(addition)
+
+#filter and map function
+#filter(function,sequence)
+seq = [1,2,3,4]
+odd = lambda x : True if x % 2 !=0 else False
+odd_from_seq = filter(odd,seq)
+print(odd_from_seq)
+print(f"odd numbers form the sequence are {list(odd_from_seq)}")
+mapped = map(lambda x : True if x % 2 !=0 else False, seq)
+print(f"map output : {list(mapped)}")
+square = map(lambda x : x ** 2, seq)
+print(f"map output : {list(square)}")
---
+++ modules_in_python.py
@@ -0,0 +1,38 @@
+# # modules in python
+# # built in modules
+# # some built in modules in python :- math, date and time, random, etc,.. 
+# # how to import a module
+# # import module
+# # importing only few function or variables : from module_name import function1, function2, function3,.... 
+ 
+# import math
+
+# #calculate the square root 
+
+# num = 100,81,64,49,36,25,16,9,4
+# square_root = math.sqrt(100)
+# print(int(square_root))
+# square_root = math.sqrt(81)
+# print(int(square_root))
+# square_root = math.sqrt(64)
+# print(int(square_root))
+# square_root = math.sqrt(49)
+# print(int(square_root))
+# square_root = math.sqrt(36)
+# print(int(square_root))
+# square_root = math.sqrt(25)
+# print(int(square_root))
+# square_root = math.sqrt(16)
+# print(int(square_root))
+# square_root = math.sqrt(9)
+# print(int(square_root))
+# square_root = math.sqrt(4)
+# print(int(square_root))
+# square_root = math.sqrt(1)
+# print(int(square_root))
+
+# Calculate the area of a circle
+import math
+r = 10
+area_of_circle = (math.pi * (r ** 2)) / 100
+print(area_of_circle)
