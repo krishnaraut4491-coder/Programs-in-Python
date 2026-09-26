b1221a0360fe2c824a481bb4df9b433ca68156b7
Krishna Raut <krishnaraut4491@gmail.com>
2026-09-26 22:53:54 +0530

    Assignment_3

--- functions_and_modules/task_1.py
+++ functions_and_modules/task_1.py
@@ -3,26 +3,33 @@ Here we are writing a program in which we will
 take input of any number and give factorial 
 of that number as output. Using loop and Recursion.
 """
-# # using loop
-# def facto(factorial):
-#     number_factorial = 1
-#     while factorial > 1:
-#         number_factorial  = number_factorial  * factorial
-#         factorial = factorial - 1
-#     return number_factorial 
+# using loop
+def facto(factorial):
+    number_factorial = 1
+    if factorial < 0:
+        return "Factorial of negative number is undefined"
+    else:
+        while factorial > 1:
+            number_factorial  = number_factorial  * factorial
+            factorial = factorial - 1
+        return number_factorial 
+
+factorial = int(input("Enter a number:"))
+print(f"Factorial of {factorial} is:")
+print(facto(factorial))
+
+# # Using recursion
+# def fact(factorial):
+#     if factorial == 1:
+#         return 1
+#     elif factorial == 0:
+#         return 1
+#     elif factorial < 0:
+#         return "undefined"
+#     else:
+#         factorial_number = factorial * fact(factorial - 1)
+#     return factorial_number 
 
 # factorial = int(input("Enter a number:"))
-# print(f"Factorial of {factorial} is:")
-# print(facto(factorial))
-
-#Using recursion
-def fact(factorial):
-    if factorial == 1:
-        return 1
-    else:
-        factorial_number = factorial * fact(factorial - 1)
-    return factorial_number 
-
-factorial = int(input("Enter a number: "))
-print(f"Factorial of {factorial} is: ")
-print(fact(factorial))
+# print(f"Factorial of {factorial} is ")
+# print(fact(factorial))
