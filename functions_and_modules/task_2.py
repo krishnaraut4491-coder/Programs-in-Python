f44a13354d5d2cc0923d03f73f0026268534bc1a
Krishna Raut <krishnaraut4491@gmail.com>
2026-09-26 22:54:28 +0530

    Assignment.3.task_2

--- functions_and_modules/task_2.py
+++ functions_and_modules/task_2.py
@@ -11,9 +11,12 @@ its square root, natural logarithm,
 sine of the number(in radians) 
 """
 n = float(input("Enter a number: "))
-print(f"Square root of {n}: {round(math.sqrt(n),5)}")
-print(f"Natural logarithm of {n}: {round(math.log(n),5)}")
-print(f"Sine of {n}: {round(math.sin(math.radians(n)),5)}")
+if n <= 0:
+    print("Enter a positive number")
+else:
+    print(f"Square root of {n}: {round(math.sqrt(n),5)}")
+    print(f"Natural logarithm of {n}: {round(math.log(n),5)}")
+    print(f"Sine of {n}: {round(math.sin(math.radians(n)),5)}")
 """
 Here I used round function to
 get only 3 floating point
